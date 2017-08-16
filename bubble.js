e7dd50cb92c9f43b0955de8341734011cfe51e3c
Bclair <bclair@gmail.com>
2017-08-16 12:26:38 -0400

    put it all in a function

--- bubble.js
+++ bubble.js
@@ -4,7 +4,7 @@ var scores = [60, 50, 60, 58, 54, 54,
               69, 64, 66, 55, 52, 61,
               46, 31, 57, 52, 44, 18,
               41, 53, 55, 61, 51, 44];
-
+function printAndGetHighScore(scores){
 var output;
 var highScore = 0;
 
@@ -15,14 +15,27 @@ for (var i = 0; i<scores.length;i++) {
         highScore = scores[i];
     }
 }
+    return highScore;
+}
+
+function getBestResults(scores, highScore) {
+    var bestSolutions = [];
+    for (var i = 0; i < scores.length; i++) {
+        if (scores[i] == highScore) {
+            bestSolutions.push(i);
+        }
+    }
+    return bestSolutions;
+}
+
+
+var highScore = printAndGetHighScore(scores);
+
 console.log("Bubbles tests: " + scores.length);
 console.log("highest bubble score: " + highScore);
 
 
-var bestSolutions = [];
-for (var i = 0; i<scores.length; i++) {
-    if (scores[i] == highScore) {
-        bestSolutions.push(i);
-    }
-}
-console.log("Solutions with the highest score: " +bestSolutions);
+var bestSolutions = printAndGetHighScore(scores);
+console.log("Solutions with the highest score: " + bestSolutions);
+console.log("Bubbles tests: " + scores.length);
+console.log("highest bubble score: " + highScore);
